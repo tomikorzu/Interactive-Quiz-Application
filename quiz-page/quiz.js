de8d45d95d863050891b4726b76f646ddb0d461a
Tomas Gentile <tomas.gentile@gm2dev.com>
2024-08-27 10:21:51 -0300

    feat(quiz.js): add show answers function

--- quiz-page/quiz.js
+++ quiz-page/quiz.js
@@ -1,4 +1,8 @@
-body.classList.add(localStorage.getItem('preferences'))
+let answers = document.querySelectorAll(".answer");
+let skipButton = document.getElementById("next-question");
+let skips = 0;
+
+skipButton.addEventListener("click", skipQuestion);
 
 let globalCategories = {
   history: [
@@ -10,6 +14,7 @@ let globalCategories = {
           "Thomas Jefferson": false,
           "Benjamin Franklin": false,
         },
+        explanation: "explicacion",
       },
       {
         "¿En qué año comenzó la Revolución Francesa?": {
@@ -18,6 +23,7 @@ let globalCategories = {
           1804: false,
           1812: false,
         },
+        explanation: "explicacion",
       },
     ],
     [
@@ -28,6 +34,7 @@ let globalCategories = {
           "El Imperio Otomano": false,
           "El Imperio Persa": false,
         },
+        explanation: "explicacion",
       },
       {
         "¿Qué evento histórico se conmemora el 4 de julio en Estados Unidos?": {
@@ -36,6 +43,7 @@ let globalCategories = {
           "La llegada de los primeros colonos a América": false,
           "La firma del Tratado de Versalles": false,
         },
+        explanation: "explicacion",
       },
     ],
     [
@@ -47,6 +55,7 @@ let globalCategories = {
             "Establecer la paz entre los estados europeos": false,
             "Reunir a los líderes de la Reforma Protestante": false,
           },
+        explanation: "explicacion",
       },
       {
         "¿Qué tratado puso fin a la Guerra de los Treinta Años en 1648?": {
@@ -55,6 +64,7 @@ let globalCategories = {
           "Tratado de Versalles": false,
           "Tratado de Paz de Oliva": false,
         },
+        explanation: "explicacion",
       },
     ],
   ],
@@ -92,16 +102,77 @@ function getQuestion(order, questions) {
   return questions[order[0]];
 }
 
-let category = getCategory("history", globalCategories);
-console.log(category);
-let questions = getDificulty(1, category[1]);
-console.log(questions);
-let order = orderQuestions(questions);
-console.log(order);
-let question;
-while (order.length > 0) {
-  question = getQuestion(order, questions);
-  console.log(question);
-  order.shift();
-  confirm(Object.keys(question));
+function getAnswer(answers) {
+  console.log(answers);
+  let answersEntries = Object.entries(answers);
+  return answersEntries.find(function () {
+    return answersEntries[1];
+  });
 }
+
+let category = getCategory("history", globalCategories);
+let questions = getDificulty(1, category[1]);
+let initialOrder = orderQuestions(questions);
+let order = initialOrder.map(function (o) {
+  return o;
+});
+let h1 = document.querySelector("h1");
+let spans = document.querySelectorAll("span");
+let userSelection = [];
+let correct = 0;
+
+function setQuestion() {
+  spans[1].textContent = correct;
+  let question = getQuestion(order, questions);
+  if (question) {
+    h1.textContent = Object.keys(question);
+    setAnswers(Object.values(question)[0]);
+    order.shift();
+  } else {
+    h1.style.display = "none";
+    answers.forEach(function (answer) {
+      answer.style.display = "none";
+    });
+  }
+}
+
+function setAnswers(question) {
+  console.log(question);
+  answers.forEach(function (answer, index) {
+    answer.textContent = Object.keys(question)[index];
+    answer.addEventListener("click", function () {
+      if (!userSelection.includes(getAnswer(question)[0])) {
+        userSelection.push(h1.textContent);
+        userSelection.push(answer.textContent);
+        console.log(question);
+        userSelection.push(getAnswer(question)[0]);
+        console.log(getAnswer(question)[0]);
+        setQuestionTransition(getAnswer(question)[0], answer.textContent);
+      }
+      if (answer.textContent == getAnswer(question)[0]) {
+        correct++;
+      } else {
+      }
+    });
+  });
+}
+
+function setQuestionTransition(correctAnswer, userAnswer) {
+  console.log(correctAnswer);
+  answers.forEach(function (answer) {
+    console.log(userAnswer);
+    if (answer.textContent == userAnswer) {
+      answer.style.backgroundColor = "red";
+    }
+    if (answer.textContent == correctAnswer) {
+      answer.style.backgroundColor = "green";
+    }
+  });
+}
+
+function skipQuestion() {
+  skips++;
+  setQuestion();
+}
+
+setQuestion();
