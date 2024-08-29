36af66a1013d3aea2bc56aaf2ae94c4f2338e1e9
Maite Sanchez <maite.sanchez@gm2dev.com>
2024-08-29 11:58:30 -0300

    fix(results.js): add a value of 0 to the default variables

--- results-page/results.js
+++ results-page/results.js
@@ -1,32 +1,27 @@
+let accumulatedPoints = localStorage.getItem('quizPoints') || 0;
+let totalQuestions = localStorage.getItem('totalQuestions') || 0;
+let incorrectAnswers = localStorage.getItem('quizFailures') || 0;
+let skippedAnswers = localStorage.getItem('quizSkips') || 0;
+let correctAnswers = JSON.parse(localStorage.getItem('correctAnswers')) || [];
+
 
 window.onload = function(){
-    let accumulatedPoints = localStorage.getItem('quizPoints');
-    let totalQuestions = localStorage.getItem('totalQuestions');
-    let incorrectAnswers = localStorage.getItem('quizFailures');
-    let skippedAnswers = localStorage.getItem('quizSkips');
-    let correctAnswers = localStorage.getItem('correctAnswers') || []; //averiguar JSON (correctAnsewers = object)
+    perfomanceList();
+    summary();
+}
 
+function perfomanceList(){
     document.getElementById('final-score').innerText = `${accumulatedPoints} / ${totalQuestions}`;
     document.getElementById('correct-count').innerText = `${accumulatedPoints}`;
     document.getElementById('incorrect-count').innerText = `${incorrectAnswers}`;
     document.getElementById('skipped-count').innerText = `${skippedAnswers}`;
+}
 
+function summary(){
     let correctAnswersSummary = document.getElementById('correct-answers-summary');
     correctAnswers.forEach(item => {
         let listItem = document.createElement('li');
         listItem.innerHTML = `<strong>Question:</strong> ${item.question} <br> <strong>Correct Answer:</strong> ${item.correctAnswer} <br> <strong>Explanation:</strong> ${item.explanation}`;
         correctAnswersSummary.appendChild(listItem);
     });
-}
-
-function restartQuiz() {
-    window.location.href = "quiz-page/index.html";
-}
-
-function goToHomePage() {
-    window.location.href = "index.html";
-}
-
-function viewLeaderboard() {
-    window.location.href = "leaderboard.html";
 }
